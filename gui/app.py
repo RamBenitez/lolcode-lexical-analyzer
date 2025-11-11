8e7964408507f13b6678aeb46a72e92fc7070716
RamBenitez <ramilimanbenitez21@gmail.com>
2025-11-11 21:59:53 +0800

    Feat:landing page for the project

---
+++ gui/app.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+from landing_page import LandingPage
+
+def main():
+    root = tk.Tk()
+    app = LandingPage(root)
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
